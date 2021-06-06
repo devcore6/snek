e99a728a6f90a4b689c5040336ba53c443d03b25
devcore6 <61387985+devcore6@users.noreply.github.com>
2021-06-06 13:37:36 +0200

    improved ai

--- kernel/kernel.cpp
+++ kernel/kernel.cpp
@@ -39,6 +39,12 @@ void spawn_apple() {
 	srand(system_timer_ms);
 }
 
+uint8_t last_dir = 0;
+
+bool skipped = false;
+bool skipping = false;
+uint8_t skip_pos_x = 0;
+
 void init_gamefield() {
 	memset(snake_data, 0, VGA_WIDTH * VGA_HEIGHT);
 	snake_length = 5;
@@ -52,6 +58,10 @@ void init_gamefield() {
 	}
 	spawn_apple();
 	dead = false;
+	last_dir = 0;
+	skipped = false;
+	skipping = false;
+	skip_pos_x = 0;
 }
 
 void draw_snake() {
@@ -71,18 +81,52 @@ void die() {
 	dead = true;
 }
 
-uint8_t last_column = 0;
-uint8_t last_dir = 0;
-
 void do_ai() {
+	bool crossing_over = false;
+	bool __crossing = false; // Only count as crossing over if at least 2 blocks are crossing
+	for(size_t i = 0; i < snake_length; i++) {
+		// Checking whether or not we're crossing over is pretty simple
+		uint8_t y = (snake_data[i] & 0x00FF);
+		if(y == VGA_HEIGHT - 1) {
+			if(__crossing) {
+				crossing_over = true;
+				break;
+			} else __crossing = true;
+		}
+	}
+	// Checking whether we skipped or not is harder, so we're gonna make it a little less efficient and just not check at all
+	// Determine how much horizontal space is left ahead of us, used to determine if we can skip an area ahead of us or not
+	uint8_t horizontal_space = 0;
+	{
+		uint8_t start_x = (snake_data[0] & 0xFF00) >> 8;
+		if(!crossing_over) {
+			horizontal_space = VGA_WIDTH - 1 - start_x;
+		} else {
+			uint8_t end_x = (snake_data[snake_length - 1] & 0xFF00) >> 8;
+			horizontal_space = end_x - start_x;
+		}
+		if(skipped) {
+			if(start_x == skip_pos_x) {
+				skipped = false;
+				skip_pos_x = 0;
+			}
+		}
+	}
 	switch(snake_direction) {
 		case 0: { // Going north - should turn east after
 			uint8_t old_x = (snake_data[0] & 0xFF00) >> 8;
 			uint8_t old_y = (snake_data[0] & 0x00FF);
-			last_column = old_x;
 			last_dir = 0;
 			if(old_y == 0) {
 				snake_direction = 3;
+				if(old_x >= skip_pos_x && !crossing_over) {
+					uint8_t apple_x = (apple & 0xFF00) >> 8;
+					if((apple_x > old_x) && ((apple_x - old_x) < horizontal_space)) {
+						skipping = true;
+						skipped = true;
+						skip_pos_x = old_x;
+					}
+				}
 			}
 			break;
 		}
@@ -96,8 +140,16 @@ void do_ai() {
 		case 2: { // Going South - should turn east/west after
 			uint8_t old_x = (snake_data[0] & 0xFF00) >> 8;
 			uint8_t old_y = (snake_data[0] & 0x00FF);
-			last_column = old_x;
 			last_dir = 2;
+			if(!crossing_over && (!skipped || skip_pos_x < old_x)) {
+				uint8_t apple_x = (apple & 0xFF00) >> 8;
+				if(apple_x < old_x) { // Apple is behind us and we're not crossing over
+					if(old_y == VGA_HEIGHT - 1) {
+						snake_direction = 1; // Early west turn to skip a lot of area
+					}
+					break;
+				}
+			}
 			if(old_x == VGA_WIDTH - 1) {
 				if(old_y == VGA_HEIGHT - 1) {
 					snake_direction = 1; // West if last column
@@ -111,7 +163,14 @@ void do_ai() {
 		}
 		case 3: { // Going East - should turn north/south after
 			uint8_t old_x = (snake_data[0] & 0xFF00) >> 8;
-			if(old_x != last_column) {
+			if(skipping) {
+				uint8_t apple_x = (apple & 0xFF00) >> 8;
+				if(apple_x % 2 == 0) apple_x--;
+				if(old_x >= apple_x) {
+					skipping = false;
+					snake_direction = 2;
+				}
+			} else {
 				if(last_dir == 2) snake_direction = 0; // Go north if previously going south
 				else snake_direction = 2; // Otherwise go south now
 			}
